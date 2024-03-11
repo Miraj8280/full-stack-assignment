9c5d3885dc0902243d72744ee2ec74b8d2b453a0
Miraj Asraf <mirajasraf786@gmail.com>
2024-03-11 22:47:12 +0530

    Deployment Setup

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,24 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import connectToMongoDB from "./database/connectToMongoDB.js";
+import path from "path";
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-app.use(express.json());
+const __dirname = path.resolve();
 
-app.get("/", (req, res) => {
-  // root route
-  res.send("Server is running!");
-});
+app.use(express.json());
 
 // auth routes
 app.use("/api/auth", authRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   connectToMongoDB();
